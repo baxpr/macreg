9108ce9e3cebbc0483946e990a8088b86e52f9ce
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2022-11-18 17:03:36 -0600

    reg-working

--- register.sh
+++ register.sh
@@ -4,7 +4,7 @@ t1_niigz=$(pwd)/INPUTS/t1.nii.gz
 t1_json=$(pwd)/INPUTS/t1.json
 acijk_csv=$(pwd)/INPUTS/t1_acijk.csv
 out_dir=$(pwd)/OUTPUTS
-nmt_dir=$(pwd)/NMT_v2.0_sym
+nmt_dir=$(pwd)/NMT_v2.0_sym/NMT_v2.0_sym_fh
 
 # Reorient and crop to make flirt happy
 echo Reorient and crop
@@ -15,6 +15,36 @@ reorient_and_crop.py \
     --position sphinx \
     --outnii "${out_dir}"/rt1.nii.gz
 
-# Reorient data ordering to make FSL happy
+# Reorder data in file to make flirt happy
 echo Reorder to standard
 fslreorient2std "${out_dir}"/rt1 "${out_dir}"/srt1
+
+# Work in the output dir now
+cd "${out_dir}"
+
+# Register. Rigid body first to keep the affine step tame. Use brain mask
+# in the affine step
+flirt \
+    -dof 6 \
+    -usesqform \
+    -in srt1 \
+    -ref "${nmt_dir}"/NMT_v2.0_sym_fh.nii.gz \
+    -out rsrt1 \
+    -omat srt1_to_nmt_6.mat
+
+flirt \
+    -usesqform \
+    -in rsrt1 \
+    -ref "${nmt_dir}"/NMT_v2.0_sym_fh.nii.gz \
+    -refweight "${nmt_dir}"/NMT_v2.0_sym_fh_brainmask.nii.gz \
+    -out arsrt1 \
+    -omat rsrt1_to_nmt.mat
+
+# Combine transformations and apply
+convert_xfm -omat srt1_to_nmt.mat -concat rsrt1_to_nmt.mat srt1_to_nmt_6.mat
+flirt \
+    -init srt1_to_nmt.mat \
+    -applyxfm \
+    -in srt1 \
+    -ref "${nmt_dir}"/NMT_v2.0_sym_fh.nii.gz \
+    -out asrt1
